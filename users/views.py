bafba8ec39cf3808497d46ff2ce0a465503117db
Luca Bonetti <lb.luca.bonetti@gmail.com>
2020-11-23 19:13:00 +0100

    Added homepage string to identify a logged user and made a basic registration form that just works

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
 
 # Create your views here.
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields
+
+
+def register(request):
+    context = {}
+    user = request.user
+
+    if user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            context['form'] = form
+    elif request.method == 'GET':
+        form = CustomUserCreationForm()
+        context['form'] = form
+
+    return render(request, 'users/register.html', context)
