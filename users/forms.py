bb8f5e427ab7df331e3fc84b896a2f5800f61b04
Luca Bonetti <lb.luca.bonetti@gmail.com>
2020-11-30 18:04:04 +0100

    Added login procedure form, view and template; modified base to avoid showing login and registration prompts when logged in

--- users/forms.py
+++ users/forms.py
@@ -1,16 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(max_length=320, label="Email", widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    username = forms.CharField(max_length=255, label="Username", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(max_length=255, label="Nome", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=255, label="Cognome", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    phone_number = forms.CharField(max_length=15, label="Numero di telefono", widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
-    birth_date = forms.DateField(label="Data di nascita", widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}), required=False)
+    email = forms.EmailField(max_length=320, label="Email", widget=forms.EmailInput(attrs={"class": "form-control"}))
+    username = forms.CharField(max_length=255, label="Username", widget=forms.TextInput(attrs={"class": "form-control"}))
+    first_name = forms.CharField(max_length=255, label="Nome", widget=forms.TextInput(attrs={"class": "form-control"}))
+    last_name = forms.CharField(max_length=255, label="Cognome", widget=forms.TextInput(attrs={"class": "form-control"}))
+    phone_number = forms.CharField(max_length=15, label="Numero di telefono", widget=forms.TextInput(attrs={"class": "form-control"}), required=False)
+    birth_date = forms.DateField(label="Data di nascita", widget=forms.DateInput(attrs={"type": "date", "class": "form-control"}), required=False)
 
     class Meta:
         model = CustomUser
@@ -18,5 +18,12 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+        self.fields["password1"].widget.attrs.update({"class": "form-control"})
+        self.fields["password2"].widget.attrs.update({"class": "form-control"})
+
+
+class CustomUserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].widget.attrs.update({"class": "form-control"})
+        self.fields["password"].widget.attrs.update({"class": "form-control"})
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from users.forms import CustomUserCreationForm
+from users.forms import CustomUserCreationForm, CustomUserLoginForm
+
 
 # Create your views here.
 
@@ -9,21 +11,44 @@ def user_register(request):
     user = request.user
 
     if user.is_authenticated:
-        return redirect('home')
+        return redirect("home")
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect("home")
         else:
-            context['form'] = form
-    elif request.method == 'GET':
+            context["form"] = form
+    elif request.method == "GET":
         form = CustomUserCreationForm()
-        context['form'] = form
+        context["form"] = form
 
-    return render(request, 'users/register.html', context)
+    return render(request, "users/register.html", context)
 
 
 def user_login(request):
-    pass
+    context = {}
+    user = request.user
+
+    if user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        form = CustomUserLoginForm(data=request.POST)
+        if form.is_valid():
+            email = request.POST["username"]
+            password = request.POST["password"]
+            user = authenticate(username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+            else:
+                context["form"] = form
+        else:
+            context["form"] = form
+    elif request.method == "GET":
+        form = CustomUserLoginForm()
+        context["form"] = form
+
+    return render(request, "users/login.html", context)
